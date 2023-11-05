5a8b54ed76c3f6f8d2423e4350dba072f4cc4d36
lsalvatierra89 <l.salvat@gmail.com>
2023-11-05 16:49:12 -0500

    métodos de registro y actualización

--- src/main/resources/static/scripts/web/administracion/frmestado.js
+++ src/main/resources/static/scripts/web/administracion/frmestado.js
@@ -1,4 +1,46 @@
 $(document).on("click", "#btnnuevo", function(){
-   //alert("Hola Javascript");
+    $("#txtdescestado").val("");
+    $("#hddidestado").val("0");
+    $("#modalregistro").modal("show");
+});
+
+$(document).on("click", ".btnactualizar", function(){
+   $("#txtdescestado").val($(this).attr("data-descestado"));
+   $("#hddidestado").val($(this).attr("data-idestado"));
    $("#modalregistro").modal("show");
 });
+
+$(document).on("click", ".btneliminar", function(){
+   $("#modalregistro").modal("show");
+});
+
+$(document).on("click", "#btnguardar", function(){
+   $.ajax({
+   type: "POST",
+   contentType: "application/json",
+   url: "/administracion/estado/registrar",
+   data: JSON.stringify({
+       idestado: $("#hddidestado").val(),
+       descestado: $("#txtdescestado").val()
+   }),
+   success: function(resultado){
+        if(resultado.respuesta){
+            listarEstados();
+        }
+        alert(resultado.mensaje);
+        $("#modalregistro").modal("hide");
+   }
+   });
+});
+
+function listarEstados(){
+    $.ajax({
+        type: "GET",
+        url: "/administracion/estado/listar",
+        dataType: "json",
+        success: function(resultado){
+            console.log(resultado);
+        }
+    });
+
+}
