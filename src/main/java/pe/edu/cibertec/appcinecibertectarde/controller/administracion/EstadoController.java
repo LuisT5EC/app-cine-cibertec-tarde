9a16a890dbdbbc4dd0de42246b0507438cb2a3c1
lsalvatierra89 <l.salvat@gmail.com>
2023-11-05 14:32:28 -0500

    actualización de controlador

--- src/main/java/pe/edu/cibertec/appcinecibertectarde/controller/administracion/EstadoController.java
+++ src/main/java/pe/edu/cibertec/appcinecibertectarde/controller/administracion/EstadoController.java
@@ -3,9 +3,12 @@ package pe.edu.cibertec.appcinecibertectarde.controller.administracion;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.appcinecibertectarde.model.bd.Estado;
+import pe.edu.cibertec.appcinecibertectarde.model.response.ResultadoResponse;
 import pe.edu.cibertec.appcinecibertectarde.service.EstadoService;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Controller
@@ -18,6 +21,22 @@ public class EstadoController {
     public String frmEstado(Model model){
         model.addAttribute("listaestados", estadoService.listarEstados());
         return "administracion/frmEstado";
+    }
+
+    @PostMapping("/registrar")
+    @ResponseBody
+    public ResultadoResponse registrarEstado(@RequestBody Estado objEstado){
+        return estadoService.registrarEstado(objEstado);
+    }
+    @GetMapping("/listar")
+    @ResponseBody
+    public List<Estado> listarEstados(){
+        return estadoService.listarEstados();
+    }
+    @DeleteMapping("/eliminar")
+    @ResponseBody
+    public ResultadoResponse eliminarEstado(@RequestBody Estado objEstado){
+        return estadoService.eliminarEstado(objEstado.getIdestado());
     }
 
 }
